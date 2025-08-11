6a53e2bb7803aa6ac4d417bce2f0f120910de0e6
smileyhead <szijartoleventepal@gmail.com>
2025-08-11 10:55:06 +0200

    Rebrand from SamplePlugin

--- SamplePlugin/Configuration.cs
+++ SamplePlugin/Configuration.cs
@@ -1,7 +1,7 @@
-﻿using Dalamud.Configuration;
+using Dalamud.Configuration;
 using System;
 
-namespace SamplePlugin;
+namespace ConditionalFocusTarget;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
