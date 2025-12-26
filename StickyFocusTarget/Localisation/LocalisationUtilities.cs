cfca86e5ae69d38035a48c9c995681b21019d352
smileyhead <szijartoleventepal@gmail.com>
2025-12-26 11:51:35 +0100

    Change language drop-down structure to "English name (native name)" and sort it alphabetically

--- StickyFocusTarget/Localisation/LocalisationUtilities.cs
+++ StickyFocusTarget/Localisation/LocalisationUtilities.cs
@@ -1,19 +1,23 @@
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace StickyFocusTarget.Localisation;
 
 public class LocalisationUtilities
 {
     public static readonly List<string> PluginSupportedLanguages = ["auto", "en", "hu", "ja"];
+    public static readonly List<string> EnglishLanguageNames = ["", "English", "Hungarian", "Japanese"];
 
     public static List<string> GenerateLanguageList()
     {
-        List<string> languageList = [Loc.settings_lang_dropdown_auto];
+        List<string> languageList = 
+            [$"{Loc.settings_lang_dropdown_auto} ({CultureInfo.GetCultureInfo(Plugin.PluginInterface.UiLanguage).NativeName})"];
 
         for (var i = 1; i < PluginSupportedLanguages.Count; i++)
-            languageList.Add(CultureInfo.GetCultureInfo(PluginSupportedLanguages[i]).NativeName);
+            languageList.Add($"{EnglishLanguageNames[i]} ({CultureInfo.GetCultureInfo(PluginSupportedLanguages[i]).NativeName})");
 
-        return languageList;
+        List<string> sortedLanguageList = languageList.Take(1).Concat(languageList.Skip(1).OrderBy(x => x)).ToList();
+        return sortedLanguageList;
     }
 }
